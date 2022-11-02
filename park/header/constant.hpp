751d40ef7202184d2ad0cb22cbfc960893ada4ff
jiyoung <jiyoung_06@ewhain.net>
2022-11-03 04:00:41 +0900

    [ADD] constant value in headerfile

---
+++ park/header/constant.hpp
@@ -0,0 +1,24 @@
+//
+// Created by JiYoung Park on 2022/10/26.
+//
+
+#ifndef PARK_CONSTANT_HPP
+#define PARK_CONSTANT_HPP
+
+
+#define FRAME_WIDTH 640
+#define FRAME_HEIGHT 360
+
+#define FRAME_ROWS FRAME_HEIGHT
+#define FRAME_COLS FRAME_WIDTH
+
+#define DX 15
+#define DEFAULT_ROI_UP 195
+#define DEFAULT_ROI_DOWN 284
+
+#define GRADIENT_STD 1.73
+
+#define SHOW
+#define DEBUG
+
+#endif //PARK_CONSTANT_HPP
