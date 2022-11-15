c6398f5e9aa02ca6d9156ed06c57b06c4945bc51
jiyoung <jiyoung_06@ewhain.net>
2022-11-16 02:54:16 +0900

    [UPDATE] SHOW & VIDEO_SAVE mode

--- wrap-up/ver2.1/main.cpp
+++ wrap-up/ver2.1/main.cpp
@@ -54,7 +54,7 @@ double getCotangent(Point p1, Point p2) {
 void drawLines(InputOutputArray frame, const std::vector <Vec4i> &lines, Scalar color = Scalar(0, 255, 0)) {
     for (Vec4i pts: lines) {
         Point p1(pts[0], pts[1]), p2(pts[2], pts[3]);
-        line(frame, p1, p2, color, 1, 8);
+        line(frame, p1, p2, color, 1, LINE_AA);
     }
 }
 
@@ -86,6 +86,13 @@ void filterLinesWithAdaptiveROI(InputOutputArray frame, const std::vector <Vec4i
         lane[i].grad = roi.line_info[i].adaptive_ROI_flag ? roi.line_info[i].get_avg().gradient : 0;
         lane[i].diff = 2.0;
         lane[i].idx = -1;
+
+        if (lane[i].grad != 0) {
+            int x1 = roi.line_info[i].get_avg().coordX;
+            int x2 = x1 - DEFAULT_ROI_HEIGHT * lane[i].grad;
+            // ROI 기준 선 빨간색으로 표시
+            line(frame, {x1, DEFAULT_ROI_DOWN}, {x2, DEFAULT_ROI_UP}, Scalar(0, 0, 255), 3, LINE_AA);
+        }
     }
 
     int idx = -1, pos; // 선분 index, pos: 왼쪽 or 오른쪽
@@ -100,7 +107,7 @@ void filterLinesWithAdaptiveROI(InputOutputArray frame, const std::vector <Vec4i
 
         if (abs(m) > GRADIENT_STD) { // 기준 기울기보다 작은 경우 (역수로 계산하므로 부등호 반대)
 #ifdef GRAPHIC
-            line(frame, p1, p2, Scalar(0, 0, 255), 1, 8);
+            line(frame, p1, p2, Scalar(0, 0, 255), 1, LINE_AA);
 #endif //GRAPHIC
             continue;
         }
@@ -113,8 +120,8 @@ void filterLinesWithAdaptiveROI(InputOutputArray frame, const std::vector <Vec4i
             lane[pos].idx = idx;
         }
 
-        // 프레임에 표기 (붉은색)
-        line(frame, p1, p2, Scalar(0, 255, 0), 1, 8);
+        // 프레임에 표기 (초록색)
+        line(frame, p1, p2, Scalar(0, 255, 0), 1, LINE_AA);
     }
 
     // 검출한 선분 update
@@ -131,12 +138,12 @@ void filterLinesWithAdaptiveROI(InputOutputArray frame, const std::vector <Vec4i
             Avg avg = roi.line_info[i].get_avg();
             int x1 = avg.coordX;
             int x2 = avg.coordX - DEFAULT_ROI_HEIGHT * avg.gradient;
-            line(frame, {x1, DEFAULT_ROI_DOWN}, {x2, DEFAULT_ROI_UP}, Scalar(255, 0, 0), 1, 8);
+            line(frame, {x1, DEFAULT_ROI_DOWN}, {x2, DEFAULT_ROI_UP}, Scalar(255, 0, 0), 1, LINE_AA);
         } else {
             // 차선 검출 성공
             Point p1(lines[lane[i].idx][0], lines[lane[i].idx][1]), p2(lines[lane[i].idx][2], lines[lane[i].idx][3]);
             // 파란색으로 표기
-            line(frame, p1, p2, Scalar(255, 0, 0), 1, 8);
+            line(frame, p1, p2, Scalar(255, 0, 0), 1, LINE_AA);
             roi.line_info[i].update_lines({calculateX1(p1, p2), getCotangent(p1, p2)});
         }
     }
@@ -163,14 +170,9 @@ void test(InputArray frame) {
     Mat grayscaled;
     cvtColor(frame, grayscaled, COLOR_BGR2GRAY);
 
-#ifdef SHOW
-    showImage("gray", grayscaled, 5);
+#if defined(SHOW) || defined(VIDEO_SAVE)
     Mat show_roi = grayscaled.clone(); // roi 마스킹 화면 출력용
-#endif // SHOW
-#ifdef VIDEO_SAVE
-    vw.writeFrame(grayscaled, 0);
-    Mat show_roi = grayscaled.clone();
-#endif //VIDEO_SAVE
+#endif //SHOW or VIDEO_SAVE
 
 #ifdef TIME_TEST
     tl.proc_record(grayscaled); // 0. to grayscale
@@ -186,15 +188,24 @@ void test(InputArray frame) {
     // 2. apply roi
     Mat roi_applied;
     roi.applyROI(grayscaled, roi_applied);
+#if defined(SHOW) || defined(VIDEO_SAVE)
+    roi.applyROI(show_roi, show_roi);
 
 #ifdef SHOW
-    roi.applyROI(show_roi, show_roi); // roi 화면 출력용
-    showImage("roi", show_roi, 5, FRAME_WIDTH);
-#endif // SHOW
+    showImage("roi", show_roi, 5);
+#endif //SHOW
 #ifdef VIDEO_SAVE
-    roi.applyROI(show_roi, show_roi);
-    vw.writeFrame(show_roi, 1);
+    vw.writeFrame(show_roi, 0);
 #endif //VIDEO_SAVE
+
+#ifdef SHOW
+    showImage("threshold", roi_applied, 5, FRAME_WIDTH);
+#endif //SHOW
+#ifdef VIDEO_SAVE
+    vw.writeFrame(roi_applied, 1);
+#endif //VIDEO_SAVE
+
+#endif //SHOW or VIDEO_SAVE
 
 #ifdef TIME_TEST
     tl.proc_record(roi_applied); // 2. apply roi
@@ -280,7 +291,11 @@ int main(int argc, char *argv[]) {
         cout << "can't find image list\n";
         return 1;
     }
-
+#ifdef VIDEO_SAVE
+    string dst_prefix = "../result/video/";
+    string cmd = "mkdir " + dst_prefix + argv[1];
+    system(cmd.c_str());
+#endif
     for (string &file_name: file_list) {
 #ifdef TIME_TEST
         tl = TimeLapse(6);
@@ -295,11 +310,14 @@ int main(int argc, char *argv[]) {
 #endif //DETECTION_RATE
 #ifdef VIDEO_SAVE
         auto pos = file_name.rfind('.');
-        string save_path = "../result/video/" + file_name.substr(pos - 2, 2) + ".mp4";
+        string save_path = dst_prefix + argv[1] + "/" + file_name.substr(pos - 2, 2) + ".avi";
         vw = OneVideoWriter(save_path, FRAME_WIDTH, FRAME_HEIGHT, 2, 2, 4);
 #endif //VIDEO_SAVE
         videoHandler(file_name);
 
+#ifdef VIDEO_SAVE
+        cout << "completed: " << save_path << '\n';
+#endif //VIDEO_SAVE
 #ifdef TIME_TEST
         tl.print_info_all();
 #endif //TIME_TEST
